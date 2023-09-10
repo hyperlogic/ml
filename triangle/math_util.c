87ff5ee2bc1e891cee6b171653b3a3eb71746ca7
Anthony J. Thibault <ajt.hyperlogic@gmail.com>
2023-09-10 09:59:30 -0700

    removed unnecessary dot_product function

--- triangle/math_util.c
+++ triangle/math_util.c
@@ -56,14 +56,6 @@ float dot_product(const float* w, const float* x, size_t count) {
     return accum;
 }
 
-// w is matrix with rows x cols elements, x is a vector with cols elements.
-// out is a vector with rows elements.
-void mat_mul_vec(float* out, const float* w, const float* x, size_t rows, size_t cols) {
-    for (size_t r = 0; r < rows; r++) {
-        out[r] = dot_product(&w[r * cols], x, cols);
-    }
-}
-
 float relu(float x) {
     return x > 0.0f ? x : 0.0f;
 }
--- triangle/math_util.h
+++ triangle/math_util.h
@@ -28,10 +28,6 @@ void tensor_comp_func(Tensor* result, const Tensor* lhs, CompFunc func);
 
 float dot_product(const float* w, const float* x, size_t count);
 
-// w is matrix with rows x cols elements, x is a vector with cols elements.
-// out is a vector with rows elements.
-void mat_mul_vec(float* out, const float* w, const float* x, size_t rows, size_t cols);
-
 float relu(float x);
 
 int argmax(const float* x, size_t count);
